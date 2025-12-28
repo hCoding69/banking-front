6687ddbd23e2529a4553656d3a769657e5fedc00
hamza03 <adkdp752@gmail.com>
2025-12-28 22:30:29 +0100

    first commit

--- src/app/services/client/client-service.ts
+++ src/app/services/client/client-service.ts
@@ -31,5 +31,30 @@ export class ClientService {
     return this.http.post<ClientRequest>(`${this.API_URL}/createClient`, clientRequest, {withCredentials: true})
   }
 
+  getClientByUserId(reqUserId: number) : Observable<ClientRequest>{
+    return this.http.get<ClientRequest>(`${this.API_URL}/getClient?reqUserId=${reqUserId}`, {withCredentials: true})
+  }
+
+  getClientByAuthentificatedUser() : Observable<ClientRequest>{
+    return this.http.get<ClientRequest>(`${this.API_URL}/getClient`, {withCredentials: true})
+  }
+
+  updateClientByUserId(clientRequest: ClientRequest, reqUserId: number): Observable<ClientRequest> {
+    return this.http.patch<ClientRequest>(`${this.API_URL}/updateClient?reqUserId=${reqUserId}`, clientRequest, {withCredentials: true})
+  }
+
+  updateClientByAuthentificatedUser(clientRequest: ClientRequest): Observable<ClientRequest> {
+    return this.http.patch<ClientRequest>(`${this.API_URL}/updateClient`, clientRequest, {withCredentials: true})
+  }
+
+  editClientStatusByUserId(clientStatus: string, reqUserId: number): Observable<ClientRequest> {
+    const data = {
+      clientStatus: clientStatus
+    }
+    return this.http.patch<ClientRequest>(`${this.API_URL}/editClientStatus?reqUserId=${reqUserId}`, data, {withCredentials: true})
+  }
+
+
+
 
 }
